436ec39f463f15af5813267da46c174442c2c706
Daria Artemova <airadavometra@gmail.com>
2024-05-09 17:01:55 +0200

    ad analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./reset.css";
 import { Layout } from "./components/Layout/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Layout />
+    <Analytics />
   </React.StrictMode>
 );
