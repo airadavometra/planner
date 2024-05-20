cc1890c0953d2cafec9d47a07c224b64a2b2b332
Daria Artemova <airadavometra@gmail.com>
2024-05-20 21:16:40 +0200

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import { Heart } from "../../icons/Heart";
+import WidthContainer from "../WidthContainer/WidthContainer";
+import s from "./Footer.module.css";
+import classNames from "classnames";
+
+export const Footer = () => {
+  return (
+    <footer className={s.footer}>
+      <WidthContainer className={s.widthContainer}>
+        <div className={s.message}>
+          <span className={s.transparent}>Made with</span>
+          <Heart className={classNames(s.heart, s.transparent)} />
+          <span className={s.transparent}>by</span>
+          <a href="https://www.airadavometra.space/" className={s.link}>
+            Daria
+          </a>
+        </div>
+        <span className={s.transparent}>2024</span>
+      </WidthContainer>
+    </footer>
+  );
+};
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import s from "./Layout.module.css";
 import { Header } from "../Header/Header";
 import { Main } from "../Main/Main";
 import { WelcomeOverlay } from "../WelcomeOverlay/WelcomeOverlay";
+import { Footer } from "../Footer/Footer";
 
 export const Layout = () => {
   return (
@@ -9,6 +10,7 @@ export const Layout = () => {
       <div id="layout" className={s.layout}>
         <Header />
         <Main />
+        <Footer />
         <WelcomeOverlay />
       </div>
     </>
