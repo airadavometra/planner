a89bac831dd2fc8ddce4a00932cb2068750f29e2
Daria Artemova <airadavometra@gmail.com>
2024-05-25 19:33:51 +0200

    use vite plugin to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,66 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["vite.svg"],
+  manifest: {
+    name: "Just a planner — minimalistic daily planner and to-do list.",
+    short_name: "Just a planner",
+    start_url: "index.html",
+    display: "standalone",
+    orientation: "portrait-primary",
+    theme_color: "#3e7bff",
+    background_color: "#dde1fb",
+    description:
+      "Just a planner is a FREE personal to-do list and daily planner to help you organise your days.",
+    icons: [
+      {
+        src: "/icon-48.png",
+        type: "image/png",
+        sizes: "48x48",
+      },
+      {
+        src: "/icon-72.png",
+        type: "image/png",
+        sizes: "72x72",
+      },
+      {
+        src: "/icon-96.png",
+        type: "image/png",
+        sizes: "96x96",
+      },
+      {
+        src: "/icon-128.png",
+        type: "image/png",
+        sizes: "128x128",
+      },
+      {
+        src: "/icon-144.png",
+        type: "image/png",
+        sizes: "144x144",
+      },
+      {
+        src: "/icon-192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "/icon-384.png",
+        type: "image/png",
+        sizes: "384x384",
+      },
+      {
+        src: "/icon-512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
