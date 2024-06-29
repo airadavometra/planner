a1c12a6dbc0179075957414eb5fb66a136f8234d
Daria Artemova <airadavometra@gmail.com>
2024-06-29 17:15:00 +0200

    exclude deleted tasks from reordering to another day

--- src/utils/moveTaskToAnotherDay.ts
+++ src/utils/moveTaskToAnotherDay.ts
@@ -8,12 +8,16 @@ export const moveTaskToAnotherDay = (
   destinationIndex: number,
   newDate: Dayjs
 ) => {
-  const sourcePlansClone = Array.from(sourcePlans);
+  const sourcePlansClone = Array.from(sourcePlans)
+    .filter((task) => !task.isDeleted)
+    .sort((a, b) => a.sortingIndex - b.sortingIndex);
   const [removed] = sourcePlansClone.splice(sourceIndex, 1);
 
   removed.date = newDate;
 
-  const destinationPlansClone = Array.from(destinationPlans);
+  const destinationPlansClone = Array.from(destinationPlans)
+    .filter((task) => !task.isDeleted)
+    .sort((a, b) => a.sortingIndex - b.sortingIndex);
   destinationPlansClone.splice(destinationIndex, 0, removed);
 
   return [sourcePlansClone, destinationPlansClone];
